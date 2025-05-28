516eeed4f75b0d536c135e2559892aa572d9ba83
nour alhoussein <nour.alhoussein@axtro-soft.com>
2025-05-28 11:25:35 +0300

    Updates

--- src/Data/Contacts.jsx
+++
@@ -1,9 +0,0 @@
-const contacts = [
-  { label: "Parent Name", inputType: "Enter Parent Name" },
-  { label: "Email Address", inputType: "Enter Email Address" },
-  { label: "Phone Number", inputType: "Enter Phone Number" },
-  { label: "Student Name", inputType: "Enter Student Name" },
-  { label: "Student Age", inputType: "Enter Student Age" },
-  { label: "Program of Intrest", inputType: "Select Program" },
-];
-export default contacts;
--- src/Data/HomeData.jsx
+++
@@ -1,6 +0,0 @@
-const items = [
-  { number: 7000, type: "Students Passed Out" },
-  { number: 37, type: "Awards & Recognitions" },
-  { number: 15, type: "Experience Educators" },
-];
-export default items ;
